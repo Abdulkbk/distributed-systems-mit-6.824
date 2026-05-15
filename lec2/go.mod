7557035463d0cddebe4bb24db6244120451c0c3f
Abdullahi Yunus <abdoollahikbk@gmail.com>
2026-05-15 02:28:13 +0100

    lecture 2 exercise on RPC and threads

---
+++ lec2/go.mod
@@ -0,0 +1,3 @@
+module github.com/abdulkbk/crawler.git
+
+go 1.25.4
---
+++ lec2/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Fetcher
+type Fetcher interface {
+	// Fetch returns a slice of URLs found on the page.
+	Fetch(url string) (urls []string, err error)
+}
+
+// fakeResult is a struct to store the body and urls of a page.
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+// fakeFetcher is a Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+func (f fakeFetcher) Fetch(url string) ([]string, error) {
+	if res, ok := f[url]; ok {
+		fmt.Printf("found: %s\n", url)
+		return res.urls, nil
+	}
+
+	fmt.Printf("missing: %s\n", url)
+	return nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+}
+
+// Serial crawls a website serially.
+func Serial(url string, fetcher Fetcher, fetched map[string]bool) {
+	// Return early if we already have fetched the url.
+	if fetched[url] {
+		return
+	}
+
+	fetched[url] = true
+
+	urls, err := fetcher.Fetch(url)
+	if err != nil {
+		return
+	}
+
+	for _, u := range urls {
+		Serial(u, fetcher, fetched)
+	}
+}
+
+// Concurrent Crawler with shared state and Mutex.
+type fetchState struct {
+	mu      sync.Mutex
+	fetched map[string]bool
+}
+
+// makeState returns fetchState
+func makeState() *fetchState {
+	f := &fetchState{}
+	f.fetched = make(map[string]bool)
+
+	return f
+}
+
+func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
+	f.mu.Lock()
+	already := f.fetched[url]
+	f.fetched[url] = true
+	f.mu.Unlock()
+
+	if already {
+		return
+	}
+
+	urls, err := fetcher.Fetch(url)
+	if err != nil {
+		return
+	}
+
+	var wg sync.WaitGroup
+
+	for _, u := range urls {
+		wg.Add(1)
+
+		go func(u string) {
+			defer wg.Done()
+			ConcurrentMutex(u, fetcher, f)
+		}(u)
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	fmt.Println("=========Serial Crawler==========")
+	Serial("http://golang.org/", fetcher, make(map[string]bool))
+
+	fmt.Println("========= Concurrent Mutex =========")
+	ConcurrentMutex("http://golang.org/", fetcher, makeState())
+}
