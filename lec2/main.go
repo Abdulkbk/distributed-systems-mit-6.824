c398f32e8de22d779f2d1b48df70fbded43c85d6
Abdullahi Yunus <abdoollahikbk@gmail.com>
2026-05-19 15:30:36 +0100

    implement all 3 methods

--- lec2/main.go
+++ lec2/main.go
@@ -97,6 +97,7 @@ func makeState() *fetchState {
 	return f
 }
 
+// ConcurrentMutex crawls a website concurrently using mutex to protect shared state.
 func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
 	f.mu.Lock()
 	already := f.fetched[url]
@@ -126,10 +127,62 @@ func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
 	wg.Wait()
 }
 
+//
+// Concurrent crawler with Channels
+//
+
+// worker
+func worker(url string, ch chan []string, fetcher Fetcher) {
+	urls, err := fetcher.Fetch(url)
+	if err != nil {
+		ch <- []string{}
+	} else {
+		ch <- urls
+	}
+}
+
+// master
+func master(ch chan []string, fetcher Fetcher) {
+	n := 1
+
+	fetched := make(map[string]bool)
+
+	for urls := range ch {
+		for _, u := range urls {
+			if fetched[u] == false {
+				fetched[u] = true
+				n += 1
+				go worker(u, ch, fetcher)
+			}
+		}
+
+		n -= 1
+
+		if n == 0 {
+			break
+		}
+	}
+
+}
+
+// ConcurrentChannel
+func ConcurrentChannel(url string, fetcher Fetcher) {
+	ch := make(chan []string)
+	go func() {
+		ch <- []string{url}
+	}()
+
+	master(ch, fetcher)
+
+}
+
 func main() {
 	fmt.Println("=========Serial Crawler==========")
 	Serial("http://golang.org/", fetcher, make(map[string]bool))
 
 	fmt.Println("========= Concurrent Mutex =========")
 	ConcurrentMutex("http://golang.org/", fetcher, makeState())
+
+	fmt.Println("========= Concurrent Channel =========")
+	ConcurrentChannel("http://golang.org/", fetcher)
 }
